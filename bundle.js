d3cf2af190a8d83b292958f4d0e32dd0b196030a
Alec Lownes <alec.lownes@gmail.com>
2016-02-25 16:49:18 -0500

    Add working interface

--- bundle.js
+++ bundle.js
@@ -1112,22 +1112,24 @@ exports.simulate = simulate;
 var trinity = require('./index.js');
 
 $(function(){
+  $('#allocation').on('change mousemove', function(){
+    $('#allocationValue').text($(this).val());
+  });
   $('#run').on('click', function(){
-    var durationYears = 50;
+    var durationYears = parseFloat($('#durationYears').val());
     var labels = [];
     for (var x = 0; x < durationYears; x++){
       labels.push(x);
     }
     var options = {
-      startingValue: 1000000,
+      startingValue: parseFloat($('#startingValue').val()),
       durationYears: durationYears,
       allocation: {
-        equities: 0.5,
-        bonds: 0.5
+        equities: parseFloat($('#allocation').val())
       },
-      fees: 0.0005,
-      spendingModel: 40000,
-      rebalance: false
+      fees: $('#fees').val(),
+      spendingModel: parseFloat($('#spending').val()),
+      rebalance: parseInt($('#rebalance').val())
     };
     var netData = {
       labels: labels,
@@ -1142,43 +1144,47 @@ $(function(){
       datasets: []
     };
     var result = trinity.simulate(options);
-    for (var y = 0; y < result.data.length; y++){
-      var runData = result.data[y];
-      var color = randomColor();
-      var netDataset = {
-        label: ''+y,
-        data: [],
-        strokeColor: color
-      };
-      var equityDataset = {
-        label: ''+y,
-        data: [],
-        strokeColor: color
-      };
-      var bondDataset = {
-        label: ''+y,
-        data: [],
-        strokeColor: color
-      };
-      for (var z = 0; z < runData.length; z++){
-        var yearData = runData[z];
-        netDataset.data.push(yearData.adjustedNet);
-        equityDataset.data.push(yearData.adjustedSharesBalance);
-        bondDataset.data.push(yearData.adjustedBondsBalance);
+    $('#successRate').text(result.successRate);
+    $('#data').show({
+      done: function(){
+        for (var y = 0; y < result.data.length; y++){
+          var runData = result.data[y];
+          var color = randomColor();
+          var netDataset = {
+            label: ''+y,
+            data: [],
+            strokeColor: color
+          };
+          var equityDataset = {
+            label: ''+y,
+            data: [],
+            strokeColor: color
+          };
+          var bondDataset = {
+            label: ''+y,
+            data: [],
+            strokeColor: color
+          };
+          for (var z = 0; z < runData.length; z++){
+            var yearData = runData[z];
+            netDataset.data.push(yearData.adjustedNet);
+            equityDataset.data.push(yearData.adjustedSharesBalance);
+            bondDataset.data.push(yearData.adjustedBondsBalance);
+          }
+          netData.datasets.push(netDataset);
+          equityData.datasets.push(equityDataset);
+          bondData.datasets.push(bondDataset);
+        }
+        var chartOptions = {
+          datasetFill: false,
+          pointDot: false,
+          showTooltips: false
+        };
+        new Chart(getContextOfId('netChart')).Line(netData, chartOptions);
+        new Chart(getContextOfId('equityChart')).Line(equityData, chartOptions);
+        new Chart(getContextOfId('bondChart')).Line(bondData, chartOptions);
       }
-      netData.datasets.push(netDataset);
-      equityData.datasets.push(equityDataset);
-      bondData.datasets.push(bondDataset);
-    }
-    var chartOptions = {
-      datasetFill: false,
-      pointDot: false,
-      showTooltips: false
-    };
-    new Chart(getContextOfId('netChart')).Line(netData, chartOptions);
-    new Chart(getContextOfId('equityChart')).Line(equityData, chartOptions);
-    new Chart(getContextOfId('bondChart')).Line(bondData, chartOptions);
-    $('#data').show();
+    });
   });
 });
 
